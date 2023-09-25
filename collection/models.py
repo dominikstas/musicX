35b36b5e3fe18725e908ff6c18bcf5c70036ad34
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-25 12:51:38 +0200

    configured collection db

--- collection/models.py
+++ collection/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
-
 class Plyta(models.Model):
-    id = models.IntegerField
-    tytul = models.CharField(max_length=255)
-    wykonawca = models.CharField(max_length=255)
-    nosnik = models.CharField(max_length=50)
-    # Dodaj inne pola modelu
+    TYPI_NOSNIKA = (
+        ('CD', 'CD'),
+        ('Vinyl', 'Vinyl'),
+    )
+
+    tytul = models.CharField(max_length=100)
+    artysta = models.CharField(max_length=100)
+    nosnik = models.CharField(max_length=10, choices=TYPI_NOSNIKA)
+    rok_zakupu = models.IntegerField()
+    zdjecie = models.CharField(max_length=1000)
+
+    # inne pola modelu
 
     def __str__(self):
         return self.tytul
--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .models import Plyta
 
 def collection(request):
-    return render(request, "collection/collection.html")
+    nosniki = ['CD', 'Vinyl']  # Dostępne opcje nośnika
+    selected_nosnik = request.GET.get('nosnik')  # Pobierz wybrany nośnik
+
+    if selected_nosnik:
+        plyty = Plyta.objects.filter(nosnik=selected_nosnik)
+    else:
+        plyty = Plyta.objects.all()
+
+    context = {
+        'plyty': plyty,
+        'nosniki': nosniki,
+        'selected_nosnik': selected_nosnik,
+    }
+
+    return render(request, 'collection/collection.html', context)
