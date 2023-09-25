a6b2beaf12e22313bc8eeee5422404f1c08b695a
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-25 11:26:28 +0200

    created collection data base

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Plyta(models.Model):
+    id = models.IntegerField
+    tytul = models.CharField(max_length=255)
+    wykonawca = models.CharField(max_length=255)
+    nosnik = models.CharField(max_length=50)
+    # Dodaj inne pola modelu
+
+    def __str__(self):
+        return self.tytul
