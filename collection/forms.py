c07cf19d384b131f02615e5947a9df7c3e811505
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-30 13:41:37 +0200

    translated into English

--- collection/forms.py
+++ collection/forms.py
@@ -5,3 +5,10 @@ class PlytaForm(forms.ModelForm):
     class Meta:
         model = Plyta
         fields = ['tytul', 'artysta', 'nosnik', 'rok_zakupu', 'zdjecie']
+        labels = {
+            'tytul': 'Title',
+            'artysta': 'Artist',
+            'nosnik': 'Media Type',
+            'rok_zakupu': 'Purchase Year',
+            'zdjecie': 'Cover Image',
+        }
